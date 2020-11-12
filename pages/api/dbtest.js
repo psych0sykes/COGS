667da26c9ee0cd3f73eb479b8286a0b36bfa8fce
psych0sykes <blakesykes13@gmail.com>
2020-11-12 11:54:01 -0700

    Updated dbtest to select all from the products table. Tested and working.

--- pages/api/dbtest.js
+++ pages/api/dbtest.js
@@ -4,7 +4,7 @@ const escape = require('sql-template-strings')
 module.exports = async (req, res) => {
   const profiles = await db.query(escape`
       SELECT *
-      FROM profiles
+      FROM products
       ORDER BY id
     `)
   res.status(200).json({ profiles})
